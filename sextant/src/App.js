17d80fe0b3e4c5bccfa794f73b45e5fdd116cb00
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-04 14:22:24 -0500

    Banner and Exhibit Components created

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,9 +1,45 @@
+import React, { Component } from 'react';
 import './App.css';
+
+// eslint-disable-next-line
+class Banner extends Component {
+  render() {
+    return (
+      <div className="container-fluid p-3 text-center">
+        <h1>Sextant</h1>
+      </div>
+    );
+  }
+}
+// eslint-disable-next-line
+class Exhibit extends Component {
+  render() {
+    return (
+      <div className="exhibit">
+        <div className="main">
+          <div className="card">
+            <div className="card-body">{this.props.name}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container-fluid">
+      <Banner />
+      <Exhibit />
+    
+    
+      <div className="footer">
+        <footer>
+
+        </footer>
+      </div>
     </div>
   );
 }
