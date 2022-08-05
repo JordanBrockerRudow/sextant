45a4e2dc2b3bcf7a18cd81a343780261d6a3733b
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-05 09:36:10 -0500

    Update App for Exhibit Container Component

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,38 +1,15 @@
-import React, { Component } from 'react';
 import './App.css';
-
-// eslint-disable-next-line
-class Banner extends Component {
-  render() {
-    return (
-      <div className="container-fluid p-3 text-center">
-        <h1>Sextant</h1>
-      </div>
-    );
-  }
-}
-// eslint-disable-next-line
-class Exhibit extends Component {
-  render() {
-    return (
-      <div className="exhibit">
-        <div className="main">
-          <div className="card">
-            <div className="card-body">{this.props.name}</div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
+import Banner from './components/Banner';
+import ExhibitContainer from './components/ExhibitContainer';
+import Exhibit from './components/Exhibit';
+import IPAddress from './components/IP';
 
 
 function App() {
   return (
     <div className="container-fluid">
       <Banner />
-      <Exhibit />
+      <ExhibitContainer />
     
     
       <div className="footer">
