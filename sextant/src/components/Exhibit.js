a3bc4b0dea3025d7656258ce0b103ff8306eeb7b
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-05 09:36:56 -0500

    Presentational and Container Components

--- sextant/src/components/Exhibit.js
+++ sextant/src/components/Exhibit.js
@@ -1,1 +1,14 @@
 import React, { Component } from 'react';
+
+// eslint-disable-next-line
+class Exhibit extends Component {
+    render() {
+      return (
+        <div className="card">
+            <div className="card-body">{this.props.name}</div>
+        </div>
+      );
+    }
+}
+
+export default Exhibit;
