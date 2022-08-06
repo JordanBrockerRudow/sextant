8e02852fb318247c3724573313700ec72dfd5f8d
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-06 09:27:50 -0500

    Initialize Dashboard main container component

---
+++ sextant/src/components/layout/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Dashboard(props) {
+    return(
+        <div className="exhibit">
+          <div className="main">
+            
+          </div>
+        </div>
+    );
+}
+
+export default Dashboard;
