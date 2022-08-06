17d1a43d30f13499e74070fb187e7644ba1053c3
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-06 09:28:06 -0500

    Layout Component for Card groups

---
+++ sextant/src/components/layout/ExhibitContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// eslint-disable-next-line
+function ExhibitContainer(card) {
+      return (
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+          <div className="col">
+
+          </div>
+        </div>
+      );
+}
+
+export default ExhibitContainer;
