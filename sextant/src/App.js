6b7982ee7b7eb7cda2d84e3f73d5b141833b5b1f
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-06 10:40:28 -0500

    Temp fix for unused no-unused-vars warning

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Banner from './components/layout/Banner';
 import ExhibitContainer from './components/layout/ExhibitContainer';
+// eslint-disable-next-line
 import Exhibit from './components/layout/Exhibit';
+// eslint-disable-next-line
 import IPAddress from './components/IP';
 
 
