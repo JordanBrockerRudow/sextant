6cb29990f669c68a0e84496fef24accaf3848045
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-06 09:29:06 -0500

    Create IPAddress component for use with ipify

--- sextant/src/components/IP.js
+++ sextant/src/components/IP.js
@@ -1,14 +1,37 @@
-import React, { Component } from 'react';
+import React  from 'react';
+import ipifyURL from '../shared/ipifyURL';
 
 // eslint-disable-next-line
-class IPAddress extends Component {
-    render() {
-      return (
-        <div className="">
-          
-        </div>
-      );
-    }
+function IPAddress() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [userIP, setIP] = useState([]);
+
+  useEffect(() => {
+    fetch(ipifyURL)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setIP(result);
+        },
+
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <p class="card-text">{userIP.ip}</p>  
+    );
+  }
 }
 
 export default IPAddress;
