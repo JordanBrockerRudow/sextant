9b1e02e48c0d10b6fb37c87ed4ab964776dfe3e6
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-06 10:37:23 -0500

    Uodate Dashboard to be main container

--- sextant/src/components/layout/Dashboard.js
+++ sextant/src/components/layout/Dashboard.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Banner from './layout/Banner';
+import ExhibitContainer from './layout/ExhibitContainer';
+
+
 
 function Dashboard(props) {
     return(
         <div className="exhibit">
           <div className="main">
-            
+            <Banner/>
+            <ExhibitContainer/>
+
           </div>
         </div>
     );
