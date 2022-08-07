3103c78fa668945536a7cd2dfe809eb966aa42e3
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-07 13:24:58 -0500

    Integrate Alternative card types

--- sextant/src/dashboard/Exhibit.js
+++ sextant/src/dashboard/Exhibit.js
@@ -2,10 +2,10 @@ import React from 'react';
 // eslint-disable-next-line
 export function Card(props) {
   return (
-    <div className="card text-white bg-dark w-50">
-      <h5 class="card-header">
+    <div className="card text-white bg-dark">
+      <h6 class="card-header">
         {props.title}
-      </h5>
+      </h6>
       <div className="card-body">
         <p class="card-text">{props.content}</p>
       </div>
@@ -16,14 +16,30 @@ export function Card(props) {
 
 
 export function Exhibit(props) {
+  if (props.extended && props.cards) {
     return (
-      <div className="row row-cols-1 row-cols-md-2 g-4">
+      <div className="row row-cols-1 row-cols-md-2 g-4 mb-4">
+        {props.cards.map((card) => (
+          <div class="col">
+            <Card title={card.title} content={card.content} footer={card.footer}/>
+          </div>
+         ))}
+      </div>
+    );
+  }
+  if (props.cards) {
+    return (
+      <div className="row row-cols-1 row-cols-md-2 g-4 mb-4">
         {props.cards.map((card) => (
           <div class="col">
             <Card title={card.title} content={card.content}/>
           </div>
          ))}
       </div>
+    );
+  }
+    return (
+      <div className="row row-cols-1 row-cols-md-2 g-4 mb-4"></div>
     );
 }
 
