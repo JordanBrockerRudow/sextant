4a8ed9594a0650d1fb8698a6177f9016f62bdaec
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-07 13:24:32 -0500

    Pylon Client and ApilUtil updated for Exhibit Card container

--- sextant/src/common/PylonClient.js
+++ sextant/src/common/PylonClient.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
-import { Card } from '../dashboard/Exhibit';
+import { Exhibit } from '../dashboard/Exhibit';
 
 const endpoint = new W3CWebSocket('ws://localhost:55455');
 
@@ -15,21 +15,32 @@ class Pylon extends Component {
       console.log('Pylon WebSocket Client Connected');
     };
     endpoint.onmessage = (message) => {
+      let sent = message.data;
       let received = Date.now();
-      let sent = message.date;
-      let latency = Math.abs(date1 - date2.valueOf());
-      this.setState({ received, sent });
-      Date.now();
-      console.log(message);
-      console.log(message.date);
+      let latency = Math.abs(received - sent);
+      this.setState({ received, sent, latency });
     };
   }
   
   render() {
+    const pylonCards = [
+      {
+        title: "Latency",
+        content: this.state.latency,
+        footer: ""
+      },
+      {
+        title: "Date Recieved",
+        content: this.state.received
+      },
+      {
+        title: "Date Sent",
+        content: this.state.sent
+      }
+    ];
     return (
       <>
-        <Card title="Latency" content={}/>
-        
+        <Exhibit extended="true" cards={pylonCards} />
       </>
     );
   }
