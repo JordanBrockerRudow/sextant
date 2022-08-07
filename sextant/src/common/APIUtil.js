3d06781bb123411be8ad30fe44a55ca8c1f50872
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-07 10:01:05 -0500

    Update APIUtil for IPv4 and Ipv6

--- sextant/src/common/APIUtil.js
+++ sextant/src/common/APIUtil.js
@@ -2,27 +2,39 @@ import React from 'react';
 // eslint-disable-next-line
 import axios from 'axios';
 //https://www.digitalocean.com/community/tutorials/react-axios-react
+// eslint-disable-next-line
+import { Card } from '../dashboard/Exhibit';
+
+
 
 export default class APIUtil extends React.Component {
   state = {
-    persons: []
+    ipv6 : "",
+    ipv4 : ""
   }
 
   componentDidMount() {
     axios.get(`https://api64.ipify.org?format=json`)
       .then(res => {
-        const persons = res.data;
-        this.setState({ persons });
+        const ipv6 = res.data.ip;
+        console.log("IPv6: ", ipv6);
+        this.setState({ ipv6 });
+      })
+
+      axios.get(`https://api.ipify.org?format=json`)
+      .then(res => {
+        const ipv4 = res.data.ip;
+        console.log("IPv4: ", ipv4);
+        this.setState({ ipv4 });
       })
   }
 
   render() {
     return (
-      <p>
-        {
-          this.state.persons.ip
-        }
-      </p>
+      <>
+        <Card title="IPv4" content={this.state.ipv4} />
+        <Card title="IPv6" content={this.state.ipv6} />
+      </>
     )
   }
 }
