cefa0c183d74fa3634b81403fb48f66b65b688a2
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-07 08:04:56 -0500

    Update App to utilize Dashboard component

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-import Banner from './components/layout/Banner';
-import ExhibitContainer from './components/layout/ExhibitContainer';
 // eslint-disable-next-line
-import Exhibit from './components/layout/Exhibit';
-// eslint-disable-next-line
-import IPAddress from './components/IP';
+import Dashboard from './dashboard/Dashboard';
 
 
 function App() {
   return (
     <div className="container-fluid">
-      <Banner />
-      <ExhibitContainer />
-    
-    
+      <Dashboard />
       <div className="footer">
         <footer>
 
