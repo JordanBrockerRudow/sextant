1269978ca14c33805c9a5f867eb630cdd5f94180
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-07 12:45:03 -0500

    Change card titles into headers

--- sextant/src/dashboard/Exhibit.js
+++ sextant/src/dashboard/Exhibit.js
@@ -3,8 +3,10 @@ import React from 'react';
 export function Card(props) {
   return (
     <div className="card text-white bg-dark w-50">
+      <h5 class="card-header">
+        {props.title}
+      </h5>
       <div className="card-body">
-        <h5 class="card-title">{props.title}</h5>
         <p class="card-text">{props.content}</p>
       </div>
     </div>
@@ -16,9 +18,11 @@ export function Card(props) {
 export function Exhibit(props) {
     return (
       <div className="row row-cols-1 row-cols-md-2 g-4">
-        <div class="col">
-          {props.content}
-        </div>
+        {props.cards.map((card) => (
+          <div class="col">
+            <Card title={card.title} content={card.content}/>
+          </div>
+         ))}
       </div>
     );
 }
