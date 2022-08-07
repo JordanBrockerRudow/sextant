832ff711afa230d7756d00b1a3249dca911c4550
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-07 07:57:18 -0500

    Integrate Websocket (Update Dependencies)

---
+++ sextant/src/common/PylonClient.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line
+class Pylon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+  }
+
+  tick() {
+    this.setState(state => ({
+      seconds: state.seconds + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        Seconds: {this.state.seconds}
+      </div>
+    );
+  }
+}
+export default Pylon;
