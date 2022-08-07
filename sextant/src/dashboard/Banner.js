5f9b1efdd65f4bc3f013325e131aa8c5599b36b9
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-07 07:59:08 -0500

    Create Banner Component with title

---
+++ sextant/src/dashboard/Banner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line
+class Banner extends Component {
+    render() {
+      return (
+        <div className="container-fluid p-3 text-center">
+          <h1>Sextant</h1>
+        </div>
+      );
+    }
+}
+
+export default Banner;
