53772366e72412e78c2e687c7d449f151092eac0
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-08-08 10:27:26 -0500

    Update Banner to utilize App Info Object

--- sextant/src/dashboard/Banner.js
+++ sextant/src/dashboard/Banner.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Sextant } from '../shared/sextantINFO';
 
 // eslint-disable-next-line
 class Banner extends Component {
     render() {
       return (
         <div className="container-fluid p-3 text-center">
-          <h1>Sextant</h1>
+          <h1>{Sextant.app.title}</h1>
         </div>
       );
     }
